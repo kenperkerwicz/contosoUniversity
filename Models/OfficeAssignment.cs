13215284d1ece54656ab0435c3cc1c9e4245ead7
unknown <kenperkerwicz@gmail.com>
2019-06-02 20:43:24 -0500

    working on successful migrations build

---
+++ Models/OfficeAssignment.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContosoUniversity.Models
+{
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    namespace ContosoUniversity.Models
+    {
+        public class OfficeAssignment
+        {
+            [Key]
+            public int InstructorID { get; set; }
+            [StringLength(50)]
+            [Display(Name = "Office Location")]
+            public string Location { get; set; }
+
+            public Instructor Instructor { get; set; }
+        }
+    }
+}
